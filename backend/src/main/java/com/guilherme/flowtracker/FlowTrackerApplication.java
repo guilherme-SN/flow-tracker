49d9eb78c11e4096f535160c805dd90877480f1a
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2024-09-10 17:25:50 -0300

    first commit

---
+++ backend/src/main/java/com/guilherme/flowtracker/FlowTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.flowtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowTrackerApplication.class, args);
+	}
+
+}
