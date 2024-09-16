90f771104e8eb7fb73109a816bf9fbcdec7e26c3
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2024-09-16 15:32:07 -0300

    feat(frontend): implement break mode

--- frontend/src/components/TimerDisplay.tsx
+++ frontend/src/components/TimerDisplay.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 export default function TimerDisplay() {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+  const [isBreakMode, setIsBreakMode] = useState(false);
 
   const startTimer = () => {
     setIsRunning(true);
@@ -18,7 +19,12 @@ export default function TimerDisplay() {
   };
 
   const stopTimer = async () => {
-    const response = await fetch(`http://localhost:8080/api/timer/calculate-break-time?focusTime=${time}`, {method: 'POST'});
+    if (isBreakMode) return;
+
+    const response = await fetch(
+      `http://localhost:8080/api/timer/calculate-break-time?focusTime=${time}`,
+      { method: "POST" },
+    );
 
     if (response.ok) {
       const textData = await response.text();
@@ -26,11 +32,13 @@ export default function TimerDisplay() {
     }
 
     setIsRunning(false);
+    setIsBreakMode(true);
   };
 
   const resetTimer = () => {
     setTime(0);
     setIsRunning(false);
+    setIsBreakMode(false);
   };
 
   useEffect(() => {
@@ -38,15 +46,29 @@ export default function TimerDisplay() {
 
     if (isRunning) {
       interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 1);
+        setTime((prevTime) => {
+          if (isBreakMode) {
+            if (prevTime <= 0) {
+              clearInterval(interval); 
+
+              setIsRunning(false);
+              setIsBreakMode(false);
+
+              return 0;
+            } else {
+              return prevTime - 1;
+            }
+          } else {
+            return prevTime + 1;
+          }
+        })
       }, 1000);
     }
 
     return () => {
       if (interval) clearInterval(interval);
     };
-  }, [isRunning, time]);
-
+  }, [isRunning, isBreakMode, time]);
 
   const formatTime = (totalSeconds: number) => {
     const hours = Math.floor(totalSeconds / 3600);
@@ -64,8 +86,24 @@ export default function TimerDisplay() {
 
   return (
     <div>
+      {/* Modes */}
+      <div className="flex flex-row justify-center gap-10 mt-10">
+        <div
+          className={`flex justify-center items-center w-[130px] h-[50px] rounded-lg text-xl font-medium select-none ${!isBreakMode ? "bg-slate-900 text-white" : "bg-slate-200"}`}
+        >
+          Focus
+        </div>
+        <div
+          className={`flex justify-center items-center w-[130px] h-[50px] rounded-lg text-xl font-medium select-none ${isBreakMode ? "bg-slate-900 text-white" : "bg-slate-200"}`}
+        >
+          Break
+        </div>
+      </div>
+
+      {/* Timer */}
       <p className="text-[125px] font-bold leading-none">{formatTime(time)}</p>
 
+      {/* Timer Controls */}
       <div className="flex flex-row justify-center gap-x-20 mt-4">
         <Button
           className="rounded-full"
