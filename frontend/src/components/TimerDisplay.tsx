26abd1ae7ceeb7c88766f54a59c211a4fbcb09b1
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2024-09-16 12:12:53 -0300

    fix: move timer control logic to application's client-side

--- frontend/src/components/TimerDisplay.tsx
+++ frontend/src/components/TimerDisplay.tsx
@@ -9,69 +9,64 @@ export default function TimerDisplay() {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
 
-  const startTimer = async () => {
-    const response = await fetch('http://localhost:8080/api/timer/start', { method: 'POST' });
+  const startTimer = () => {
+    setIsRunning(true);
+  };
 
-    if (response.ok) {
-      setIsRunning(true);
+  const pauseTimer = () => {
+    setIsRunning(false);
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  const resetTimer = () => {
+    setTime(0);
+    setIsRunning(false);
+  };
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
     }
-  };
 
-  const pauseTimer = async () => {
-    const response = await fetch('http://localhost:8080/api/timer/pause', { method: 'POST' });
-    if (response.ok) {
-      setIsRunning(false);
-    }
-  };
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [isRunning, time]);
 
-  const stopTimer = async () => {
-    const response = await fetch('http://localhost:8080/api/timer/stop', { method: 'POST' });
-    if (response.ok) {
-      setIsRunning(false);
-      setTime(0);
-    }
-  };
-
-  const resetTimer = async () => {
-    const response = await fetch('http://localhost:8080/api/timer/reset', { method: 'POST' });
-
-    if (response.ok) {
-      setTime(0); 
-      setIsRunning(false);
-    }
-  };
-
-  const fetchStatus = async () => {
-    const response = await fetch("http://localhost:8080/api/timer/status");
-    const data = await response.json();
-
-    setTime(data.time);
-    setIsRunning(data.running);
-  };
 
   const formatTime = (totalSeconds: number) => {
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
 
-    const formattedHours = hours > 0 ? String(hours).padStart(2, '0') : null;
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(seconds).padStart(2, '0');
+    const formattedHours = hours > 0 ? String(hours).padStart(2, "0") : null;
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    const formattedSeconds = String(seconds).padStart(2, "0");
 
-    return formattedHours ? `${formattedHours}:${formattedMinutes}:${formattedSeconds}` : `${formattedMinutes}:${formattedSeconds}`;
+    return formattedHours
+      ? `${formattedHours}:${formattedMinutes}:${formattedSeconds}`
+      : `${formattedMinutes}:${formattedSeconds}`;
   };
-
-  useEffect(() => {
-    const interval = setInterval(fetchStatus, 1000);
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <div>
       <p className="text-[125px] font-bold leading-none">{formatTime(time)}</p>
 
       <div className="flex flex-row justify-center gap-x-20 mt-4">
-        <Button className="rounded-full" variant="ghost" size="icon" onClick={isRunning ? pauseTimer : startTimer}>
+        <Button
+          className="rounded-full"
+          variant="ghost"
+          size="icon"
+          onClick={isRunning ? pauseTimer : startTimer}
+        >
           <Image
             src={isRunning ? "icons/pause-circle.svg" : "icons/play-circle.svg"}
             alt={isRunning ? "Pause Icon" : "Play Icon"}
@@ -79,7 +74,12 @@ export default function TimerDisplay() {
             height={40}
           />
         </Button>
-        <Button className="rounded-full" variant="ghost" size="icon" onClick={stopTimer}>
+        <Button
+          className="rounded-full"
+          variant="ghost"
+          size="icon"
+          onClick={stopTimer}
+        >
           <Image
             src="icons/stop-circle.svg"
             alt="Stop Icon"
@@ -87,7 +87,12 @@ export default function TimerDisplay() {
             height={40}
           />
         </Button>
-        <Button className="rounded-full" variant="ghost" size="icon" onClick={resetTimer}>
+        <Button
+          className="rounded-full"
+          variant="ghost"
+          size="icon"
+          onClick={resetTimer}
+        >
           <Image
             src="icons/timer-reset.svg"
             alt="Reset Timer Icon"
