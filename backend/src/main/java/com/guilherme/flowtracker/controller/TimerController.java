6147746aac51c42978a77f14837a551545fc52be
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2024-09-22 14:49:48 -0300

    chore: change cors configuration to config/CorsConfig.java

--- backend/src/main/java/com/guilherme/flowtracker/controller/TimerController.java
+++ backend/src/main/java/com/guilherme/flowtracker/controller/TimerController.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpSession;
 import com.guilherme.flowtracker.service.TimerService;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/timer")
-@CrossOrigin(origins = "http://localhost:3000")
 public class TimerController {
     private final TimerService timerService;
 
