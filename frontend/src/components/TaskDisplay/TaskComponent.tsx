17f08ff39da50227c43ff0731a8e1d3edcf9b322
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2024-10-01 14:06:02 -0300

    feat: implement dropdown menu for task list and for each task

--- frontend/src/components/TaskDisplay/TaskComponent.tsx
+++ frontend/src/components/TaskDisplay/TaskComponent.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-
-import styles from "./Task.module.css"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import styles from "./Task.module.css";
 import { useState } from "react";
 
 interface TaskProps {
@@ -10,7 +15,11 @@ interface TaskProps {
   isCompleted: boolean;
 }
 
-const TaskComponent: React.FC<TaskProps> = ({ id, description, isCompleted }) => {
+const TaskComponent: React.FC<TaskProps> = ({
+  id,
+  description,
+  isCompleted,
+}) => {
   const [completed, setCompleted] = useState<boolean>(isCompleted);
 
   const toggleCompletion = async () => {
@@ -19,10 +28,10 @@ const TaskComponent: React.FC<TaskProps> = ({ id, description, isCompleted }) =>
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ 
+      body: JSON.stringify({
         id: id,
         description: description,
-        isCompleted: !completed
+        isCompleted: !completed,
       }),
     });
 
@@ -31,30 +40,66 @@ const TaskComponent: React.FC<TaskProps> = ({ id, description, isCompleted }) =>
     }
 
     setCompleted((prevCompleted) => !prevCompleted);
-  }
+  };
 
   return (
     <div className="flex flex-row items-center justify-between bg-slate-100 rounded-lg h-[55px] mt-5">
       <div className="flex flex-row items-center">
-        <Button className={`ml-2 rounded-full ${styles.buttonAnimation}`} variant="ghost" size="icon">
+        <Button
+          className={`ml-2 rounded-full ${styles.buttonAnimation}`}
+          variant="ghost"
+          size="icon"
+        >
           <Image
-            src={completed ? "icons/checked-circle.svg" : "icons/check-circle-2.svg"}
+            src={
+              completed
+                ? "icons/checked-circle.svg"
+                : "icons/check-circle-2.svg"
+            }
             alt="Check Icon"
             width={40}
             height={40}
             onClick={toggleCompletion}
           />
         </Button>
-        <p className={`transition ease delay-1 text-xl p-0 m-0 ml-2 ${completed ? "line-through text-muted-foreground" : ""}`}>
+        <p
+          className={`transition ease delay-1 text-xl p-0 m-0 ml-2 ${completed ? "line-through text-muted-foreground" : ""}`}
+        >
           {description}
         </p>
       </div>
-      <Image
-        src="icons/Meatballs_menu.svg"
-        alt="Meatballs Menu Icon"
-        width={40}
-        height={40}
-      />
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Image
+            src="icons/Meatballs_menu.svg"
+            alt="Meatballs Menu Icon"
+            width={40}
+            height={40}
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>
+            <Image
+              className="mr-1"
+              src="icons/edit.svg"
+              alt="Edit Icon"
+              width={20}
+              height={20}
+            />
+            Edit Task
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Image
+              className="mr-1"
+              src="icons/trash-bin.svg"
+              alt="Trash Bin Icon"
+              width={20}
+              height={20}
+            />
+            Delete Task
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
