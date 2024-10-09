167e49812450eea56ac3c64ef70b55fd56e91c43
Guilherme Nascimento <guilhermesouzanastoo@hotmail.com>
2024-10-09 18:51:02 -0300

    docs: add documentation for timer controller

--- backend/src/main/java/com/guilherme/flowtracker/controller/TimerController.java
+++ backend/src/main/java/com/guilherme/flowtracker/controller/TimerController.java
@@ -10,8 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/timer")
+@Tag(name = "Timer Controller", description = "Operations related to the timer")
 public class TimerController {
     private final TimerService timerService;
 
@@ -19,6 +25,10 @@ public class TimerController {
         this.timerService = timerService;
     }
 
+    @Operation(summary = "Calculate break time")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved the break time")
+    })
     @PostMapping("calculate-break-time")
     public ResponseEntity<Integer> getBreakTime(@RequestParam("focusTime") Integer focusTime, HttpSession session) {
         Float multiplier = (Float) session.getAttribute("breakTimeMultiplier");
